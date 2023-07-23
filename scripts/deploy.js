590f87282d6ee37ee33e182eedc580e403935695
D. Alfred Ostrowski <115842612+dalfredostrowski@users.noreply.github.com>
2023-07-23 19:05:39 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,27 +3,13 @@
 const hre = require("hardhat");
 
 async function main() {
-  console.log("deploying...");
-//1  const FlashLoan = await hre.ethers.getContractFactory("FlashLoan");
-
-//0x0496275d34753A48320CA58103d5220d394FF77F
 // contract address is from the aave site "
 //  const flashLoan = await FlashLoan.deploy(
  //   "<pool provider>"
-//  );
-//1  const flashLoan = await FlashLoan.deploy("0x0496275d34753A48320CA58103d5220d394FF77F");
-//1  await flashLoan.deployed();
-
+//  )
  const deployedContract = await hre.ethers.deployContract("FlashLoan",["0x0496275d34753A48320CA58103d5220d394FF77F"]);
-
  await deployedContract.waitForDeployment();
-
  console.log("SimpleStorage Contract Address:", await deployedContract.getAddress())
-
-
-
-
-//  console.log("Flash loan contract deployed: ", deployedContract.address);
 }
 
 main().catch((error) => {
